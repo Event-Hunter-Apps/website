48e8c61b20e4464bbdf4854da9197ad6d974ad46
restuarachman <restuarachman@gmail.com>
2022-04-12 15:40:35 +0700

    edit image for login register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,23 +19,20 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<img src="images/1.png" alt="coba">
+					<img src="assets/img/1.png" alt="coba">
 				</div>
 			</div>
 			<div class="row justify-content-center">
 				<div class="col-md-12 col-lg-10">
 					<div class="wrap d-md-flex">
-						<div class="img" style="background-image: url(images/tripplane.png);object-fit: fill;"></div>
+						<div class="img" style="background-image: url(assets/img/tripplane.png);object-fit: fill;"></div>
 						<div class="login-wrap p-4 p-md-5">
 					<div class="d-flex">
 						<div class="w-100">
 							<h3 class="mb-4">Sign In</h3>
 						</div>
 							<div class="w-100">
-								<p class="social-media d-flex justify-content-end">
-									<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-									<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-								</p>
+								
 							</div>
 						</div>
 							<form action="/login" method="POST">
@@ -52,7 +49,7 @@
 									<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
 								</div>
 		          			</form>
-		          			<p class="text-center">Have no account? <a href="./register">Sign Up</a></p>
+		          			<p class="text-center">Have no account? <a href="/register">Sign Up</a></p>
 		       			</div>
 		      		</div>
 				</div>
