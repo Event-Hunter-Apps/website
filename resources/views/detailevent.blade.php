72f5febce6e2a744cbb7d79d8f26ffde91250933
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-04-14 01:21:20 +0700

    update : detailEvent.blade.php

--- resources/views/detailevent.blade.php
+++ resources/views/detailevent.blade.php
@@ -26,6 +26,7 @@
 <body>
 
     <div class="container">
+        <h1><b>Detail Event</b></h1>
         <div class="card-detail-event">
             <div class="wrapper-detail-event row">
                 <div class="preview-detail-event">
@@ -49,24 +50,41 @@
                 </div>
                 <div class="details-detail-event">
                     <h2 class="product-title-detail-event"><b>Monas Jakarta</b></h2><br>
-                    <h4 class="harga-detail-event" style="margin-top: -10px;"><b>Rp 25.000</b></h4>
+
+                    <!-- <h4 class="harga-detail-event" style="margin-top: -10px;"><b>Rp 25.000</b></h4> -->
+                    <label for="">Date</label>
                     <div class="date-input-detail-event" style="margin-top: 5px;">
-                        <input type="text" class="form-control datepicker1" id="start_date" name="start_date" value="" />
-                        <span class="line-date-detail-event"></span>
-                        <input type="text" class="form-control datepicker1" id="end_date" name="end_date" value="" />
+                        <input type="text" class="form-control datepicker1" id="start_date" name="start_date" value="START" disabled />
+
+                        <input type="text" class="form-control datepicker1" id="end_date" name="end_date" value="END" disabled />
 
                     </div><br>
+                    <label for="">Time</label>
                     <div class="time-input-detail-event">
+
                         <!-- <input type="time" name="time-start" id="time1">
                         <span class="line-time-detail-event"></span>
                         <span><input type="time" name="time-end" id="time2" style="margin-left: 65px;"></span> -->
-                        <input type="text" class="form-control time-pickable" id="start_time" name="start_time" value="" />
-                        <span class="line-time-detail-event"></span>
-                        <input type="text" class="form-control time-pickable" id="start_time" name="start_time" value="" />
+                        <input type="text" class="form-control time-pickable" id="start_time" name="start_time" value="START" disabled />
+
+                        <input type="text" class="form-control time-pickable" id="start_time" name="start_time" value="END" disabled />
                     </div> <br>
-                    <div class="col-position-detail-event">Jln. Kuningan Jakarta Pusat
-                        <!-- <input type="text" name="pos-detail-event" id="pos-edit" value="Jln. Kuningan Jakarta Pusat"> -->
-                    </div> <br>
+                    <div class="col-location">
+                        <label for="">Location</label><br>
+                        <textarea class="event-name-decoration" name="" id="" cols="30" rows="10" disabled required>{{ isset($event)?$event->deskripsi:'INI ISI LOKASI'}}</textarea>
+
+                    </div>
+                    <br>
+                    <div>
+                        <label for="">City</label><br>
+                        <input class="event-city-decoration @error('kota') is-invalid @enderror" type="text" name="kota" id="pos-edit" value="{{ isset($event)?$event->kota:'CITY' }}" disabled>
+                    </div>
+                    @error('kota')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <br>
+                    <br>
+
                     <ul class="tags-detail-event">
                         <li class="tag-detail-event">Fun Activities</li>
                         <li class="tag-detail-event">Explorer Indonesia</li>
@@ -78,42 +96,12 @@
         <div class="description-detail-event">
             <div class="description-title-detail-event">
                 <b>Description</b>
-                <div class="border-description-detail-event">
-                    <p>Monas Jakarta</p><br>
-                    <p>Nge-date Bareng Event Hunter</p><br>
-                    <p>11-14 Februari 2021 <br> Yuk beli tiket event/attraction ini di Event Hunter tgl 11-14 Februari untuk dapatkan promo special buat berdua + Extra DISC 50% dengan kode promo berikut: <br> yoyoyyyy <br> DISC 50% max 20k <br> Kuota terbatas!<br>
-                    </p><br>
-                    <p>*Syarat & ketentuan berlaku </p>
-                </div>
+
+                <textarea class="event-name-decoration" name="" id="" cols="30" rows="10" disabled required>{{ isset($event)?$event->deskripsi:'INI ISI DESKRIPSI'}}</textarea>
+
             </div>
         </div>
-        <div class="description-detail-event" style="margin-top: 20px">
-            <div class="term-cond-title-detail-event">
-                <b>Term and Condition</b>
-                <div class="border-term-cond-detail-event">
-                    <p>Detail Tiket:
-                    </p><br>
-                    <p>Special Identitas Jakarta - Weekday
-                    </p><br>
-                    <p>Berlaku untuk pengunjung yang memiliki kartu Identitas Jakarta (KTP / SIM/KIPEM, mohon tunjukan saat kedatangan) <br> Berlaku dari hari Senin - Jumat (Tidak berlaku untuk Libur Nasional) <br> Special Identitas Bali - Weekend
-                    </p><br>
-                    <p>Berlaku untuk pengunjung yang memiliki kartu Identitas Jakarta (KTP / SIM/KIPEM, mohon tunjukan saat kedatangan) <br> Berlaku dari hari Sabtu, Minggu, dan Libur Nasional <br> Regular - Domestic & KITAS
-                    </p><br>
-                    <p>Berlaku untuk pengunjung Domestic seluruh Indonesia dan pemegang KITAS <br> Tiket dapat digunakan periode weekday, weekend, dan Libur Nasional <br> Elephant Mud Fun - Domestic & KITAS</p> <br>
-                    <p>Berlaku untuk pengunjung Domestic seluruh Indonesia dan pemegang KITAS <br> Tiket dapat digunakan periode weekday, weekend, dan Libur Nasional <br> Khusus program Elephant Mud Fun, anak dipebolehkan mengikuti program ini dengan minimum
-                        tinggi 120 cm dan akan dikenakan harga dewasa <br> Syarat dan Ketentuan Umum:
-                    </p> <br>
-                    <p>Monumen nasional buka setiap hari dari pukul 09:00 AM - 06:00 PM, check-in terakhir pukul 05:00 PM <br> Tiket berlaku sampai 31 October 2021 <br> Usia anak antara 2 - 12 tahun. Bayi dibawah 2 tahun gratis biaya masuk <br> Jika cuaca
-                        buruk, jadwal pertunjukan dapat berubah tanpa pemberitahuan sebelumnya. <br> Cara menggunakan tiket: <br> Tunjukan voucher pada konter tiket dengan di print atau menggunakan HP <br> Hanya Voucher yang kami kirimkan melalui email
-                        anda yang berlaku. Kwitansi atau bukti pembayaran tidak dapat digunakan untuk masuk. <br> Tiket yang sudah dibeli tidak dapat dikembalikan <br> Syarat dan Ketentuan Pengunjung:
-                    </p> <br>
-                    <p>1. Pengunjung harus dalam keadaan sehat </p>
-                    <p>2. Wajib sudah vaksinasi minimal dosis 1</p>
-                    <p>3. Menunjukan sertifikat vaksinasi melalui aplikasi Peduli Lindungi atau bisa dicetak </p>
-                    <p>4. Mematuhi Protokol Kesehatan di venue</p>
-                </div>
-            </div>
-        </div>
+
         <div class="btn-disc-save-detail-event">
             <a href="/category">
                 <button id="btn-save-detail-event"><b>Check Availability</b></button>
