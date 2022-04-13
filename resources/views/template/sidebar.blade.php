52bfa372fc448833f6b8fd5e6e356c8c75c972e0
irwanrezaf <irwanrezaf10@gmail.com>
2022-04-14 03:35:12 +0700

    sidebar

---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>iPortfolio Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <!-- <link href="assets/css/style.css" rel="stylesheet"> -->
+
+  <!-- =======================================================
+  * Template Name: iPortfolio - v3.7.0
+  * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+    .jumbotron {
+      margin-top: 5rem;
+      margin-left: 12rem;
+      width: 400%;
+      height: 600px;
+      background-image: url('notfon.png');
+      background-repeat: no-repeat;
+      background-size: contain;
+    }
+
+    .ml-4 {
+      margin-left: 12rem;
+    }
+  </style>
+
+</head>
+
+
+
+<!-- ======= Mobile nav toggle button ======= -->
+<i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+
+
+<body>
+
+  <div class="row">
+    <div class="col-3">
+      <!-- ======= Header ======= -->
+      <!-- <div id="header" class="row">
+        <div class="">
+
+          <nav id="navbar" class="nav-menu navbar col">
+            <ul>
+              <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+              <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+              <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+            </ul>
+          </nav>
+        </div>
+      </div> -->
+  
+    </div>
+    <div class="col-8">
+      <div class="d-flex flex-column container text-center">
+        <img src="notfon.png" class="mx-auto mt-3" width="500" height="400"  alt="">
+        <h1 style="color:#D3CFCF; font-family:poppins">Data Not Found</h1>
+        <p style="font-family:poppins">We are unable to find the data that you are looking for. </p>
+        <p>Please add some data to review or edit data</p>
+      </div>
+    </div>
+  </div>
+  
+  
+</body>
+
