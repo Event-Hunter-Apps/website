b58d4ba90595c989453131362ca501d45673d1be
restuarachman <restuarachman@gmail.com>
2022-04-18 02:34:09 +0700

    making seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Tiket;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::insert(
+            array(
+                array('role_id' => "1", "email" => "restuarachan@gmail.com", "password" => bcrypt("admin"), 'nama' => 'restu', 'no_hp'=>'08112311617'),
+                array('role_id' => "1", "email" => "restuarachan@gmail.com", "password" => bcrypt("admin"), 'nama' => 'restu', 'no_hp'=>'08112311617'),
+                array('role_id' => "1", "email" => "restuarachan@gmail.com", "password" => bcrypt("admin"), 'nama' => 'restu', 'no_hp'=>'08112311617'),
+            )
+        );
+
+        
     }
 }
